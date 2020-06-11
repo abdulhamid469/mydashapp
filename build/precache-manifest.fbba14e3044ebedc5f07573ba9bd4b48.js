f493421a068d913d4cca2454c0e86929f147b43a
abdulhamid469 <37768968+abdulhamid469@users.noreply.github.com>
2020-06-11 13:00:57 +0500

    Add files via upload

---
+++ build/precache-manifest.fbba14e3044ebedc5f07573ba9bd4b48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ca0998a4d76c00150abaa359f4e0cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "191f87bbbeaffc264886",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47a5d2356f866a744013",
+    "url": "/static/js/2.6d0716df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d0716df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191f87bbbeaffc264886",
+    "url": "/static/js/main.b11ab26a.chunk.js"
+  },
+  {
+    "revision": "14d3e3f95eef6a4ce553",
+    "url": "/static/js/runtime-main.ebdadd67.js"
+  }
+]);
